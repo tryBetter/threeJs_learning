24aab4b0612d14dda8ee6b13ea3fe8e765b66228
刘武 <w@lw.local>
2018-01-09 00:40:58 +0800

    初始化学习示例代码。

---
+++ js/common.js
@@ -0,0 +1,18 @@
+document.body.style.margin = "0px";
+document.body.style.padding = "0px";
+document.body.style.overflow = "hidden";
+
+var pointData = [
+    new THREE.Vector3(-15, 0, 0)
+];
+
+var lineData = [
+    new THREE.Vector3(-10, -10, 0),
+    new THREE.Vector3(-5, 10, 0)
+];
+
+var faceData = [
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(7, -10, 0),
+    new THREE.Vector3(15, 10, 0)
+];
